4a256e2392b93c21012beffb073b383d19cb558d
FIC <fic@info.local>
2025-07-31 20:28:11 -0300

    Aprendendo Java

---
+++ JAVA/Fundamentos/src/Main.java
@@ -0,0 +1,86 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    //main ele é obrigatorio sempre ter.
+    public static void main(String[] args) {
+        //Esse codigo abaixo faz x
+
+        //Escreve uma mensagem na tela do console
+        //System.out.printf("Hello and welcome!");
+
+        //Atalho sout - pra criar um print, mensagem na tela
+        System.out.println("");
+
+        /* VARIAVEIS
+         Toda variavel no JAVA tem que ter um tipo e um nome
+
+         Tipo de variaveis
+
+         int - números inteiros;
+         ex.: int idade = 22
+
+         long - Números inteiros, porém somente para números inteiros.
+         E tem que sempre terminar com um L no final do número inteiro. Ele ocupa muita memoria então a melhor
+         opção é colocar se realmente precisar.
+         ex.: long populacao = 80000000000000000L
+
+         double - números decimais, números quebrados. Padrão americano, que seria sempre usado "."
+         ex.: double preco = 20.90;
+
+         float - Ele é decimal também, porém ele cabe mais números que o double.
+         ex.: float altura = 1.76f;
+
+         String - Ele é usado para textos, tendo que ter "" duplas. Não pode colocar aspas simples '', por que é
+         usado no carteres. É obrigatorio ter S maisculo.
+         ex.: String nome = "Vinicio";
+
+         char - Seria para caracter, guardando caracteres. Podendo guardar somente uma letra. Ele é usado com aspas
+         simples ''.
+         ex.: char letra = 'A'
+
+         boolean - Seria o SIM ou NÃO
+         ex.: boolean maiorDeIdade = true; //podendo ser false também.
+         */
+
+        /*
+        OPERADORES
+        Os operadores logicos ele serve para comparar, concatenar, atribuir e contas lógicas.
+
+        = - Ele seria o valor de algo, ele vai atribuir alguma coisa pra algo.
+        ex.: int idade = 22
+
+        + - Seria um operador aritmético, servindo pra fazer contas.
+        ex.: int soma = 8 + 5;
+
+        - - para subtrair.
+        ex.: int subtracao = 8 - 5;
+
+        * - multiplicação
+        ex.: int multiplicacao = 8 * 2;
+
+        / - divisão
+        ex.: int divisao = 10 / 2;
+
+        % - resto. ELe é o famoso true e falso da conta aritmetica.
+        ex.: int resto = 10 % 2;
+
+        == - Seria o igual =.
+        ex.: System.out.println(10 == 6);
+
+        < - menor.
+        ex.: System.out.println(10 < 2)
+
+        > - maior.
+        ex.: System.out.println(2 > 10)
+
+        => - Seria igual ou maior.
+        ex.: System.out.pritnln(10 => 10)
+
+        =< - Seria igual ou menor.
+        ex.: System.out.pritnln(10 =< 10)
+
+        != - Ele informa que se for diferente.
+        ex.: System.out.println(10 != 5)
+         */
+    }
+}
